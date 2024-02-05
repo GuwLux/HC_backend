0a9db8bac6bb98a023e66194c35534fa2c07ab20
GuwLux <135597744+GuwLux@users.noreply.github.com>
2024-02-05 17:32:58 +0800

    Initial commit

--- server.js
+++ server.js
@@ -12,6 +12,31 @@ mongoose.connect('mongodb+srv://scsc13579113:Qaz13579113@hcweb.mwb3cmu.mongodb.n
   useUnifiedTopology: true,
 });
 
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const Product = mongoose.model('Product', productSchema);
+
 // Middleware to parse JSON requests
 app.use(express.json());
 
@@ -25,7 +50,7 @@ app.post('/api/products', upload.single('imageFile'), async (req, res) => {
     const { name, price, type, description } = req.body;
     const imageBuffer = req.file.buffer.toString('base64');
 
-    const newProduct = new productSchema({ name, price, image: imageBuffer, type, description });
+    const newProduct = new Product({ name, price, image: imageBuffer, type, description });
     const savedProduct = await newProduct.save();
 
     res.json(savedProduct);
@@ -37,7 +62,7 @@ app.post('/api/products', upload.single('imageFile'), async (req, res) => {
 
 app.get('/api/products', async (req, res) => {
   try {
-    const products = await productSchema.find();
+    const products = await Product.find();
     res.json(products);
   } catch (error) {
     console.error(error);
@@ -48,7 +73,7 @@ app.get('/api/products', async (req, res) => {
 app.get('/api/products/:id', async (req, res) => {
   try {
     const productId = req.params.id;
-    const product = await productSchema.findById(productId);
+    const product = await Product.findById(productId);
 
     if (!product) {
       return res.status(404).json({ error: 'Product not found' });
@@ -59,9 +84,9 @@ app.get('/api/products/:id', async (req, res) => {
       _id: product._id,
       name: product.name,
       price: product.price,
-      imageUrl: `data:image/jpeg;base64,${product.image}`, // 使用商品圖片的 base64 字串
       type: product.type,
       description: product.description,
+      imageUrl: `data:image/jpeg;base64,${product.image}`, // 使用商品圖片的 base64 字串
     };
 
     res.json(productWithImageUrl);
@@ -74,7 +99,7 @@ app.get('/api/products/:id', async (req, res) => {
 app.delete('/api/products/:id', async (req, res) => {
   try {
     const productId = req.params.id;
-    await productSchema.findByIdAndDelete(productId);
+    await Product.findByIdAndDelete(productId);
     res.json({ message: 'Product deleted successfully' });
   } catch (error) {
     console.error(error);
@@ -84,7 +109,7 @@ app.delete('/api/products/:id', async (req, res) => {
 
 app.get("/test", (req, res) => {
   res.json({
-    message:"test work!!",
+    message: "test work!!",
   });
 });
 
