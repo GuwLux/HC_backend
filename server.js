3eb68588c0a47f619dbe8d521a716ccc1effa6b2
GuwLux <135597744+GuwLux@users.noreply.github.com>
2024-02-05 16:20:31 +0800

    Initial commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 
